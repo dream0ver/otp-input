ef78e5d060636e527db6423c4ec80ff80df54f7c
dream0ver <krishnachaithanya826@gmail.com>
2023-03-18 20:19:05 +0530

    updated logic

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const inputs = [...document.getElementsByTagName("input")];
 const fetchInputs = () => {
-  const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("keyup", (e) => {
-      if (NUMBERS.includes(e.key) && i < inputs.length - 1) {
+      const isNumber = e.key.charCodeAt(0) > 47 && e.key.charCodeAt(0) < 58;
+      if (isNumber && i < inputs.length - 1) {
         inputs[i + 1].focus();
-      } else if (e.key == "Backspace" && i > 0) {
-        inputs[i - 1].focus();
-      } else if (!NUMBERS.includes(e.key)) {
+      } else if (e.key == "Backspace") {
         inputs[i].value = "";
+        if (i > 0) {
+          inputs[i - 1].focus();
+        }
+      }
+    });
+    inputs[i].addEventListener("keydown", (e) => {
+      const isNumber = e.key.charCodeAt(0) > 47 && e.key.charCodeAt(0) < 58;
+      if (!isNumber) {
+        e.preventDefault();
       }
     });
   }
